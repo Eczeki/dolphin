9773c4b05b157a9fce7b1475dc49ed4deecc9599
Jas Laferriere <Fizzi36@gmail.com>
2019-01-21 23:24:48 -0500

    change window title to Slippi

--- Source/Core/Common/Version.cpp
+++ Source/Core/Common/Version.cpp
@@ -18,16 +18,18 @@ namespace Common
 #define BUILD_TYPE_STR ""
 #endif
 
-const std::string scm_rev_str = "Dolphin "
-#if !SCM_IS_MASTER
-                                "[" SCM_BRANCH_STR "] "
-#endif
+//const std::string scm_rev_str = "Dolphin "
+//#if !SCM_IS_MASTER
+//                                "[" SCM_BRANCH_STR "] "
+//#endif
 
-#ifdef __INTEL_COMPILER
-    BUILD_TYPE_STR SCM_DESC_STR "-ICC";
-#else
-    BUILD_TYPE_STR SCM_DESC_STR;
-#endif
+const std::string scm_rev_str = "Dolphin Slippi r12";
+
+//#ifdef __INTEL_COMPILER
+//    BUILD_TYPE_STR SCM_DESC_STR "-ICC";
+//#else
+//    BUILD_TYPE_STR SCM_DESC_STR;
+//#endif
 
 const std::string scm_rev_git_str = SCM_REV_STR;
 const std::string scm_desc_str = SCM_DESC_STR;
@@ -35,10 +37,10 @@ const std::string scm_branch_str = SCM_BRANCH_STR;
 const std::string scm_distributor_str = SCM_DISTRIBUTOR_STR;
 
 #ifdef _WIN32
-const std::string netplay_dolphin_ver = SCM_DESC_STR " Win";
+const std::string netplay_dolphin_ver = "Slippi r12 Win";
 #elif __APPLE__
-const std::string netplay_dolphin_ver = SCM_DESC_STR " Mac";
+const std::string netplay_dolphin_ver = "Slippi r12 Mac";
 #else
-const std::string netplay_dolphin_ver = SCM_DESC_STR " Lin";
+const std::string netplay_dolphin_ver = "Slippi r12 Lin";
 #endif
 }  // namespace Common
